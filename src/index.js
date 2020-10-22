ae5fe60d631bdddab5049914f93b1b80ae7c95be
Vitali Siniak <43549823+nepp503@users.noreply.github.com>
2020-10-22 15:27:25 +0200

    init: added reverse int functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let number = n.toString();
+  let reverseNumber = "";
+  for(let i=number.length-1; i>=0; i--){
+      reverseNumber += number[i];
+  }
+  return parseInt(reverseNumber);
 }
